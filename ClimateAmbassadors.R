2a8b52468a24195c74a54d963162c6bcc4f60b8c
Blair Wong <blairwwong@gmail.com>
2023-05-03 09:49:12 -0400

    Adding data sources

--- ClimateAmbassadors.R
+++ ClimateAmbassadors.R
@@ -12,12 +12,14 @@ open_space<-st_read('datasets/OpenSpace/OpenSpace.shp')
 ltm_income<-st_read('datasets/Low_to_Moderate_Income_Population_by_Tract_v2/Low_to_Moderate_Income_Population_by_Tract.shp')
 somerville_ct<-read.csv('datasets/somerville_census_tracts.csv')
 
+# Append 25025 to census tract IDs for other census tracts
+somerville_ct$CENSUS_TRACT_ID = paste('25017', somerville_ct$CENSUS_TRACT_ID, sep="")
+
 somerville_ct_list<-as.list(somerville_ct$CENSUS_TRACT_ID)
 
 # Filter
 ltm_income<-ltm_income %>%
-  filter(TRACT %in% somerville_ct_list)
-
+  filter(GEOID %in% somerville_ct_list)
 
 # Maps
 somerville<-get_map(location=c(left = -71.15, 
@@ -28,7 +30,7 @@ somerville<-get_map(location=c(left = -71.15,
 somerville_map<-ggmap(somerville)
 somerville_map
 
-ltm_income_map<-somerville_map + geom_sf(data=ltm_income[ltm_income$LOWMODPCT>0,], aes(fill=LOWMODPCT), inherit.aes = FALSE) +
-  scale_fill_gradient(high = "purple", low = "orange") + 
-  labs(fill = "Percentage of Low to Moderate Income")
+ltm_income_map<-somerville_map + geom_sf(data=ltm_income[ltm_income$LOWMODPCT>0,], aes(fill=LOWMODPCT), alpha = 0.8, inherit.aes = FALSE) +
+  scale_fill_gradient(high = "purple", low = "orange") +
+  labs(fill = "Percentage of \nLow to Moderate Income")
 ltm_income_map
